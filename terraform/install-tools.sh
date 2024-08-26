323f88be619db552f67ac445e3513c461461ee82
jyothi <thotajyothi19@gmail.com>
2024-08-26 12:00:44 +0530

    terraformfile

--- terraform/install-tools.sh
+++ terraform/install-tools.sh
@@ -11,6 +11,7 @@ sudo yum install git -y
 sudo dnf install java-11-amazon-corretto -y
 
 #------------jenkins install-------------
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install jenkins -y
@@ -31,16 +32,17 @@ sudo yum -y install terraform
 #cd bin
 #chmod +x startup.sh
 
-
-
 #---------------------------Maven install -------------
 sudo yum install maven -y
 
 #---------------------------kubectl install ---------------
+
 sudo curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin 
+
 # -----------------------------eksctl install--------------------------------
+
 sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
@@ -53,7 +55,7 @@ tar -zxvf helm-v3.6.0-linux-amd64.tar.gz
 
 sudo mv linux-amd64/helm /usr/local/bin/helm
 
-chmod 777 /usr/local/bin/helm  # give permissions
+chmod 777 /usr/local/bin/helm  # To give permissions
 
 #------------------Docker install-------------
 #sudo amazon-linux-extras install docker #linux 2022
@@ -89,9 +91,10 @@ helm install stable prometheus-community/kube-prometheus-stack -n prometheus
 echo "Initialization script completed successfully."
 
 #----------------------sonarQube install-----------------------------------
-#sudo yum -y install wget nfs-utils
-#sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
-#sudo yum -y install sonar
+sudo yum -y install wget nfs-utils
+sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
+sudo yum -y install sonar
+
 #-----------------------JFROg-----------------------------
 #sudo wget https://releases.jfrog.io/artifactory/artifactory-rpms/artifactory-rpms.repo -O jfrog-artifactory-rpms.repo;
 
