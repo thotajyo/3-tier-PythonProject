7c803c67264fd3e3417a294a5c701e7b6c1024b9
jyothi <thotajyothi19@gmail.com>
2024-08-27 22:12:26 +0530

    newfile-terraform

--- terraform/iam-instance-profile.tf
+++
@@ -1,8 +0,0 @@
-data "aws_iam_instance_profile" "existing" {
-  name = "naresh-veera-profile"
-}
-
-
-
-
-
--- terraform/jumphost.tf
+++ terraform/jumphost.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2" {
   key_name               = var.key_name
   subnet_id              = aws_subnet.public-subnet1.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
-  iam_instance_profile = data.aws_iam_instance_profile.existing.name
+
   root_block_device {
     volume_size = 30
   }
@@ -13,4 +13,5 @@ resource "aws_instance" "ec2" {
   tags = {
     Name = var.instance_name
   }
+    iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 }
